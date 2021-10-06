87349471187a9e92880bf37a2b8721b345ec4366
Anthony Tran <anthonytran173@gmail.com>
2021-10-06 09:30:13 -0700

    Completed almostIncreasingSequence

---
+++ almostIncreasingSequence.js
@@ -0,0 +1,11 @@
+const almostIncreasingSequence = sequence => {
+  let invalidNumbers = 0;
+  for (let i = 1; i < sequence.length; i++) {
+    if (sequence[i] <= sequence[i - 1]) {
+      invalidNumbers++;
+      if (invalidNumbers > 1) return false;
+      if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) return false;
+    }
+  }
+  return true;
+}
